ed70430b4cd724a05114ca405d58f654f8e5894b
Lewaashaaban <shaabanlewaa@gmail.com>
2024-01-16 03:17:53 +0200

    Final update

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -3,30 +3,32 @@ import "./CSS/LoginSignup.css";
 import { Link } from "react-router-dom";
 import { auth } from "../firebase"; // Import the auth object from your firebase.js file
 import CustomModal from "../Components/Alert/Custommodal";
+import { useNavigate } from "react-router-dom";
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showLoginModal, setShowLoginModal] = React.useState(false);
   const [errormsg, setErrorMessage] = React.useState(false);
+  const [succeedMessage, setSucceedMessage] = React.useState(false);
+  const navigate = useNavigate();
   var err = "";
   const handleSignup = () => {
     if (!email || !password) {
       setShowLoginModal(true);
-
       return;
     }
-
     auth
       .createUserWithEmailAndPassword(email, password)
       .then((userCredentials) => {
         const user = userCredentials.user;
-        console.log("User signed up with ", user.email);
+        navigate("/");
+        alert("you Signed up successfully !!");
+        setSucceedMessage(true);
       })
       .catch((error) => {
         console.error("Error signing up:", error);
-        // alert(error.message);
+        alert(error.message);
         err = error.message;
-        setErrorMessage(true);
       });
   };
 
@@ -68,8 +70,13 @@ const Signup = () => {
       <CustomModal
         isOpen={errormsg}
         onRequestClose={() => setErrorMessage(false)}
-        message={err}
+        message="Please check you email validity and try again"
       />
+      {/* <CustomModal
+        isOpen={succeedMessage}
+        onRequestClose={() => setSucceedMessage(false)}
+        message="You Signed Up Successfully !!"
+      /> */}
     </div>
   );
 };
